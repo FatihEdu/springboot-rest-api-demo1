c5ab725d69bd734df092dd5656fc92e2df90e1ff
FTH <canbekliodev@gmail.com>
2025-07-08 14:20:10 +0300

    Move ApiControllers to its own package (controller)

--- src/main/java/com/example/fth/fth_demo/controller/ApiControllers.java
+++ src/main/java/com/example/fth/fth_demo/controller/ApiControllers.java
@@ -1,4 +1,4 @@
-package com.example.fth.fth_demo;
+package com.example.fth.fth_demo.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,4 +12,6 @@ public class ApiControllers {
     public String getHomePage() {
         return "Hello World?";
     }
+
+    
 }
