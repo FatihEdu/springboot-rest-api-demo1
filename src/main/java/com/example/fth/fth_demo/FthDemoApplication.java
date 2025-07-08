227b6a96a5d4cf046c892fd47500b7887bdbcc90
FTH <canbekliodev@gmail.com>
2025-07-08 13:29:04 +0300

    First.

---
+++ src/main/java/com/example/fth/fth_demo/FthDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.fth.fth_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FthDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FthDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/fth/fth_demo/FthDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fth.fth_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FthDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
