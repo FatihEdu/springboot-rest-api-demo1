3ee32dd30be3b31bc0016c2be906c45a2b1668cf
FTH <canbekliodev@gmail.com>
2025-07-13 11:03:01 +0300

    Enhances ReviewDto by adding Lombok annotations for builder pattern and constructors, improving code readability and usability.

--- src/main/java/com/example/fth/fth_demo/dto/ReviewDto.java
+++ src/main/java/com/example/fth/fth_demo/dto/ReviewDto.java
@@ -11,9 +11,15 @@ import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Null;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data 
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class ReviewDto {
 
